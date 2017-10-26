dec6b25f9496aaaa7a7ddad480dba758365253cc
Tanaes <jonsan@gmail.com>
2017-10-26 12:23:42 -0700

    added -w parameter

--- barnacle/barnacle_fail.sh
+++ barnacle/barnacle_fail.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-snakemake -j 1 --cluster 'qsub -l nodes=1:ppn=1 -l mem=1 -l walltime=00:00:10 ' --cluster-status barnacle/barnacle_status.py
+snakemake -j 1 -w 90 --cluster 'qsub -l nodes=1:ppn=1 -l mem=1 -l walltime=00:00:10 -o /dev/null -e /dev/null ' --cluster-status barnacle/barnacle_status.py
